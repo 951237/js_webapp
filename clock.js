3aeb23fa2fee91825f95e1163bcdcfcf9560f869
951237 <951237@gmail.com>
2021-01-31 02:22:03 +0900

    Doc : Update - 시계형식 조정함 - 숫자가 9보다 작은 경우 앞에 0을 붙여주기 : mini if문 사용 - 1초마다 시간을 업데이트하기 : setInterval() 함수 사용

--- clock.js
+++ clock.js
@@ -7,11 +7,13 @@ function getTime() {
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`
+    // 숫자가 10보자 작은경우 숫자 앞에 0을 붙여주기 , mini if 문
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
 }
 
 function init() {
     getTime();
+    setInterval(getTime, 1000);
 }
 
 init();
