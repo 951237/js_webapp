0cc8992b9dd2dc2de27338d8e4c93e6a87d3ab7b
951237 <951237@gmail.com>
2021-02-03 01:40:08 +0900

    Doc : Update - todo 삭제후 로컬스토리지에 재저장 - let과 const의 차이점 - 재할당의 가능 여부 - filter 함수 - 조건에 맞게 리스트를 재생성

--- todo.js
+++ todo.js
@@ -4,7 +4,21 @@ const toDoForm = document.querySelector(".js-toDoForm"), // form 구문 배당
 
 const TODO_LS = "toDos"; // 로컬스토리지에 변수이름 설정
 
-const toDos = []; // 리스트 생성
+// const와 let의 차이점
+// let - 변수 재선언 불가 but 재할당은 가능
+// const - 변수 재선언, 재할당 모두 불가능
+let toDos = []; // 리스트 생성 const => let의 차이, toDos를 재할당 해야해서 선언을 바꿈.
+
+function deleteToDo(event) { // todo 지우기
+    const btn = event.target; // 클릭한 목표를 btn에 저장
+    const li = btn.parentNode; // 버튼의 부모를 li로 저장
+    toDoList.removeChild(li); // ul태그의 li를 지우기
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id); // toDos리시트 내의 값의 id와 li의 id가 같지 않은 것만 추출해서 리스트로 생성하기 filter() 함수의 활용
+    });
+    toDos = cleanToDos; // 새롭게 생성한 리스트를 todos리스트로 새롭게 저장
+    saveToDos(); // toDos를 로컬스토리지에 저장하기
+}
 
 function saveToDos() {
     localStorage.setItem(TODO_LS, JSON.stringify(toDos)); // 배열의 값을 텍스트 형태로 로컬스토리지에 저장.
@@ -17,6 +31,7 @@ function paintToDo(text) { // todo 만들기 함수
     const span = document.createElement("span"); // span 태그 생성
     const newId = toDos.length + 1; // 변수를 배열의 갯수 + 1로 저장
     delBtn.innerText = "X"; // 버튼에 텍스트값 넣기
+    delBtn.addEventListener("click", deleteToDo); // 클릭하면 할일지우기
     span.innerText = text; // span에 텍스트 넣기
     li.appendChild(delBtn); // li태그밑에 버튼 태그 생성, 순서 1
     li.appendChild(span); // li태그 밑에 span 태그 생성, 순서 2
